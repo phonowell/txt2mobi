44e63f43bcdff002081fcf492f4dba08a10e0741
Mimiko Phonowell <phonowell@gmail.com>
2024-04-08 12:14:35 +0800

    Update dependencies in package.json and task/update.ts

--- task/update.ts
+++ task/update.ts
@@ -1,6 +1,11 @@
-import { exec } from 'fire-keeper'
+import { exec, read } from 'fire-keeper'
 
 // interface
+
+type Pkg = {
+  dependencies?: Record<string, string>
+  devDependencies?: Record<string, string>
+}
 
 type Result = {
   current: string
@@ -10,23 +15,45 @@ type Result = {
   dependencyType: 'dependencies' | 'devDependencies'
 }
 
-// function
+// functions
+
+const getListLocked = async () => {
+  const pkg = await read<Pkg>('./package.json')
+  if (!pkg) return []
+
+  const listLocked = [
+    ...Object.entries(pkg.dependencies ?? {}),
+    ...Object.entries(pkg.devDependencies ?? {}),
+  ]
+    .filter(it => !Number.isNaN(Number(it[1][0])))
+    .map(it => it[0])
+
+  return listLocked
+}
 
 const main = async () => {
+  await updateDeps()
+}
+
+const updateDeps = async () => {
   const [, raw] = await exec('pnpm outdated --json')
   const result = JSON.parse(raw) as Record<string, Result>
 
+  const listLocked = await getListLocked()
   const listName = Object.entries(result)
     .filter(it => {
-      const [, data] = it
+      const [name, data] = it
       if (data.isDeprecated) return false
+      if (listLocked.includes(name)) return false
+      if (name.endsWith('react') || name.endsWith('react-dom')) return false
       return true
     })
     .map(it => it[0])
   if (!listName.length) return
 
   const listCmd = listName.map(name => `pnpm i ${name}@latest`)
-  await exec(listCmd)
+  await exec([...listCmd])
+  await exec('pnpm update')
 }
 
 // export
